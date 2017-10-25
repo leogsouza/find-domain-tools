ef49384323502daa3bf622fbbd2a84032c6e6d62
Leonardo Souza <leogsouza@gmail.com>
2017-10-25 19:53:06 +0000

    Implement domainify tool

---
+++ domainify/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+	"unicode"
+)
+
+var tlds = []string{"com", "net"}
+
+const allowedChars = "abcdefghijklmnopqrstuvwxyz01234567890_-"
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+
+	for s.Scan() {
+		text := strings.ToLower(s.Text())
+		var newText []rune
+		for _, r := range text {
+			if unicode.IsSpace(r) {
+				r = '-'
+			}
+			if !strings.ContainsRune(allowedChars, r) {
+				continue
+			}
+			newText = append(newText, r)
+		}
+		fmt.Println(string(newText) + "." + tlds[rand.Intn(len(tlds))])
+	}
+}
